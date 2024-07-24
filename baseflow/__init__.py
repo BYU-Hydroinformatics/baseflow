bdf36616fe048271a0cf66b1092c39104debfde4
xueyil9 <xueyi@XueyiAir.app.byu.edu>
2024-07-23 20:57:19 -0600

    Commit local changes before pulling remote changes

---
+++ baseflow/__init__.py
@@ -0,0 +1,6 @@
+from baseflow.separation import *
+from baseflow.param_estimate import *
+from baseflow.comparision import *
+from baseflow.utils import *
+
+example = Path(__file__).parent / 'example.csv'

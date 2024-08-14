d1e5b8aecbdb509208df3dd7b3ed73e75115f02b
xueyil9 <xueyi@XueyiAir.app.byu.edu>
2024-08-13 23:19:14 -0600

    delete the comparison

--- baseflow/__init__.py
+++ baseflow/__init__.py
@@ -1,6 +1,5 @@
 from baseflow.separation import *
 from baseflow.estimate import *
-from baseflow.comparison import *
 from baseflow.utils import *
 
 example = Path(__file__).parent / 'example.csv'

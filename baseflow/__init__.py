ec71e269827b78342f3b698404c080882588f013
xueyil9 <xueyi@xueyiair.lan>
2024-08-19 10:32:48 -0600

    comment import multi_stations

--- baseflow/__init__.py
+++ baseflow/__init__.py
@@ -1,5 +1,5 @@
 from baseflow.separation import *
 from baseflow.estimate import *
 from baseflow.utils import *
-from .separation import multi_stations
+# from .separation import multi_stations
 example = Path(__file__).parent / 'example.csv'
